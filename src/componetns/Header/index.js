c335946fba8a717d27e9cdf1f3e9f7638ab1281b
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-03-05 11:48:25 -0300

    Add name user logged in the header

--- src/componetns/Header/index.js
+++ src/componetns/Header/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
-import { isAuthenticated } from '../../services/auth';
+import { getToken, isAuthenticated } from '../../services/auth';
 import './styles.css';
 import logo from '../../logo.svg';
+import jwt from 'jsonwebtoken';
 
 export default function Header(){
+
+  const [firstName, setFirstname] = useState("GLEYSON");
+  const [lastName, setLastname] = useState("");
+
+  useEffect(() => {
+    if(isAuthenticated()){
+      let { id, firstname, lastname, email } = jwt.decode(getToken()).data;
+      setFirstname(firstname);
+      setLastname(lastname);
+    }
+  }, []);
+  
 
   return (
     <div className="header">
     <div className="infos">
     <img src={logo} alt="Loading" />
-    <h3>GLEYSON</h3>
+  <h3 className="userName">{`${firstName} ${lastName}`}</h3>
     </div>
       <div>
         <Link className="link-add" to={"/users/add"}>Add user</Link>
--- src/services/auth.js
+++ src/services/auth.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 export const TOKEN_KEY = "@apirest-Token";
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
-export const getToken = () => {
-  localStorage.getItem(TOKEN_KEY);
-}
+export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const login = token => {
   localStorage.setItem(TOKEN_KEY, token);
 }
